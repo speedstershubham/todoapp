db1a194d4c0101d4f7aa27be839a11d6de19537f
Shubham Chowksay <shubhamchowksay10@gmail.com>
2020-10-05 22:00:07 +0530

    first commit

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ require('dotenv').config()
 const express = require("express")
 const app = express()
 const mongoose = require("mongoose")
+const cors = require("cors")
+
 
 mongoose.connect(process.env.DATABASE,{useNewUrlParser:true,useUnifiedTopology: true})
 const db = mongoose.connection
@@ -10,6 +12,7 @@ db.on('error',(error) => console.error(error))
 db.once('open',() => console.log('connected to Database'))
 const port = process.env.PORT || 2000;
 
+app.use(cors())
 app.use(express.json())
 
 const Todo = require("./routes/Todos")
